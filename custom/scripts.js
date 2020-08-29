8850feaeeda73f6663df8e981aa4e1b2331d2eb0
tarikkavaz <tarikkavaz@gmail.com>
2020-08-29 18:33:32 +0300

    Montserrat

--- custom/scripts.js
+++ custom/scripts.js
@@ -7,17 +7,9 @@ $(document).ready(function () {
     $("[data-toggle=popover]").popover({
         trigger: 'focus'
     })
-
 });
 
 function scrollSmoothTo(elementId) {
     var element = document.getElementById(elementId);
     element.scrollIntoView({ block: 'start',  behavior: 'smooth' });
 }
-// $(window).on("scroll", function () {
-//     if ($(window).scrollTop() >= 200) {
-//         $("#nav-index").addClass("compressed");
-//     } else {
-//         $("#nav-index").removeClass("compressed");
-//     }
-// });
