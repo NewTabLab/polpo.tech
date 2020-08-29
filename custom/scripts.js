a996457227215f413cbc600f3ebfacff2e4803a7
tarikkavaz <tarikkavaz@gmail.com>
2020-08-29 18:25:41 +0300

    scrollSmoothTo

--- custom/scripts.js
+++ custom/scripts.js
@@ -8,9 +8,12 @@ $(document).ready(function () {
         trigger: 'focus'
     })
 
-
-
 });
+
+function scrollSmoothTo(elementId) {
+    var element = document.getElementById(elementId);
+    element.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+}
 // $(window).on("scroll", function () {
 //     if ($(window).scrollTop() >= 200) {
 //         $("#nav-index").addClass("compressed");
